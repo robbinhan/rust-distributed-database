1cc882eb2c750f9034f6d8efd516d08c4b4b82d0
韩斌 <hanb@kingnet.com>
2019-01-31 18:16:57 +0800

    push

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,3 @@
-// extern crate bodyparser;
-// extern crate iron;
-// extern crate router;
-// extern crate time;
-// // extern crate wal_rs;
-
-// use iron::prelude::*;
-// use iron::status;
-// use iron::{typemap, AfterMiddleware, BeforeMiddleware};
-// use router::Router;
-// use time::precise_time_ns;
-// // use wal_rs::*;
-
-// struct ResponseTime;
-
-// impl typemap::Key for ResponseTime {
-//     type Value = u64;
-// }
-
-// impl BeforeMiddleware for ResponseTime {
-//     fn before(&self, req: &mut Request) -> IronResult<()> {
-//         req.extensions.insert::<ResponseTime>(precise_time_ns());
-//         Ok(())
-//     }
-// }
-
-// impl AfterMiddleware for ResponseTime {
-//     fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
-//         let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
-//         println!("Request took: {} ms", (delta as f64) / 1000000.0);
-//         Ok(res)
-//     }
-// }
-
-// fn hello_world(req: &mut Request) -> IronResult<Response> {
-//     let ref query = req
-//         .extensions
-//         .get::<Router>()
-//         .unwrap()
-//         .find("msg")
-//         .unwrap_or("/");
-
-//     // let body = req.get::<bodyparser::Raw>();
-//     // match body {
-//     //     Ok(Some(body)) => println!("Read body:\n{}", body),
-//     //     Ok(None) => println!("No body"),
-//     //     Err(err) => println!("Error: {:?}", err),
-//     // }
-
-//     // let json_body = req.get::<bodyparser::Json>();
-//     // match json_body {
-//     //     Ok(Some(json_body)) => println!("Parsed body:\n{:?}", json_body),
-//     //     Ok(None) => println!("No body"),
-//     //     Err(err) => println!("Error: {:?}", err),
-//     // }
-
-//     // let struct_body = req.get::<bodyparser::Struct<MyStructure>>();
-//     // match struct_body {
-//     //     Ok(Some(struct_body)) => println!("Parsed body:\n{:?}", struct_body),
-//     //     Ok(None) => println!("No body"),
-//     //     Err(err) => println!("Error: {:?}", err),
-//     // }
-
-//     Ok(Response::with((status::Ok, *query)))
-// }
-
-// // const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
-
-// fn main() {
-//     // let cfg = Config {
-//     //     entry_per_segment: 100,
-//     //     check_crc32: false,
-//     // };
-
-//     // let mut wal = WAL::open("./testdir", cfg).unwrap();
-
-//     let mut router = Router::new(); // Alternative syntax:
-
-//     let mut chain = Chain::new(hello_world);
-//     chain.link_before(ResponseTime);
-//     // chain.link_before(Read::<bodyparser::MaxBodyLength>::one(MAX_BODY_LENGTH));
-//     chain.link_after(ResponseTime);
-
-//     router.post("/task", chain, "index"); // let router = router!(index: get "/" => handler,
-
-//     Iron::new(router).http("localhost:3000").unwrap();
-// }
-
 extern crate actix;
 extern crate actix_web;
 extern crate bytes;
@@ -110,6 +22,9 @@ use futures::{Future, Stream};
 use bytes::Buf;
 use wal_rs::*;
 
+use std::sync::Arc;
+use std::sync::Mutex;
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Task {
     name: String,
@@ -122,9 +37,14 @@ fn new_task(req: &HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Erro
         .and_then(|val: Task| {
             println!("model: {:?}", val);
 
-            // let mut buf = Cursor::new(val.name.as_bytes());
+            let mut buf = Cursor::new(val.name.as_bytes());
 
-            // req.state().wal.write(&[buf.get_u8()]).unwrap();
+            req.state()
+                .wal
+                .lock()
+                .unwrap()
+                .write(&[buf.get_u8()])
+                .unwrap();
 
             Ok(HttpResponse::Ok().json(val)) // <- send response
         })
@@ -133,13 +53,13 @@ fn new_task(req: &HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Erro
 
 /// Application state
 struct AppState {
-    wal: WAL,
+    wal: Arc<Mutex<WAL>>,
 }
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    let sys = actix::System::new("json-example");
+    let sys = actix::System::new("server");
 
     let cfg = Config {
         entry_per_segment: 100,
@@ -148,8 +68,12 @@ fn main() {
 
     let mut wal = WAL::open("./testdir", cfg).unwrap();
 
-    server::new(|| {
-        App::with_state(AppState { wal })
+    let wal = Arc::new(Mutex::new(wal));
+
+    server::new(move || {
+        // App::with_state(app_state)
+
+        App::with_state(AppState { wal: wal.clone() }) // <- create app with shared state
             // enable logger
             .middleware(middleware::Logger::default())
             .resource("/task", |r| r.method(http::Method::POST).f(new_task))
@@ -162,3 +86,86 @@ fn main() {
     println!("Started http server: 127.0.0.1:8080");
     let _ = sys.run();
 }
+
+// #![cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
+// //! There are two level of statefulness in actix-web. Application has state
+// //! that is shared across all handlers within same Application.
+// //! And individual handler can have state.
+// //!
+// //! > **Note**: http server accepts an application factory rather than an
+// //! application > instance. Http server constructs an application instance for
+// //! each thread, > thus application state
+// //! > must be constructed multiple times. If you want to share state between
+// //! different > threads, a shared object should be used, e.g. `Arc`.
+// //!
+// //! Check [user guide](https://actix.rs/book/actix-web/sec-2-application.html) for more info.
+
+// extern crate actix;
+// extern crate actix_web;
+// extern crate env_logger;
+// extern crate futures;
+// extern crate serde_json;
+
+// #[macro_use]
+// extern crate serde_derive;
+// extern crate json;
+
+// use std::sync::Arc;
+// use std::sync::Mutex;
+
+// use futures::{Future, Stream};
+
+// use actix_web::{
+//     http, middleware, server, App, AsyncResponder, Error, HttpMessage, HttpRequest, HttpResponse,
+//     Json,
+// };
+
+// /// Application state
+// struct AppState {
+//     counter: Arc<Mutex<usize>>,
+// }
+
+// #[derive(Debug, Serialize, Deserialize)]
+// struct Task {
+//     name: String,
+// }
+
+// /// simple handle
+// fn index(req: &HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Error = Error>> {
+//     req.json()
+//         .from_err() // convert all errors into `Error`
+//         .and_then(|val: Task| {
+//             println!("model: {:?}", val);
+
+//             // let mut buf = Cursor::new(val.name.as_bytes());
+
+//             // req.state().wal.write(&[buf.get_u8()]).unwrap();
+
+//             Ok(HttpResponse::Ok().json(val)) // <- send response
+//         })
+//         .responder()
+// }
+
+// fn main() {
+//     ::std::env::set_var("RUST_LOG", "actix_web=info");
+//     env_logger::init();
+//     let sys = actix::System::new("ws-example");
+
+//     let counter = Arc::new(Mutex::new(0));
+//     //move is necessary to give closure below ownership of counter
+//     server::new(move || {
+//         App::with_state(AppState {
+//             counter: counter.clone(),
+//         }) // <- create app with shared state
+//         // enable logger
+//         .middleware(middleware::Logger::default())
+//         // register simple handler, handle all methods
+//         .resource("/", |r| r.f(index))
+//     })
+//     .bind("127.0.0.1:8080")
+//     .unwrap()
+//     .start();
+
+//     println!("Started http server: 127.0.0.1:8080");
+//     let _ = sys.run();
+// }
